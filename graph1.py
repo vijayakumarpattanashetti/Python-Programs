3a76d7a90fa90ab08a73249735bc820cfae84689
vijayakumarpattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2018-04-13 10:50:56 +0530

    Add files via upload

---
+++ graph1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [6,12,18,24,30,36]
+y = [15,30,45,60,85,100]
+
+plt.xlabel('This is the horizontal axis')
+plt.ylabel('This is the vertical axis')
+plt.title('kya re graph ah?')
+
+plt.plot(x,y)
+plt.show()
