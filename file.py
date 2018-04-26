b90b173b603ba1456237629b5609b51f7fdd4cd7
vijayakumarpattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2018-04-26 12:16:45 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,23 +1,18 @@
 #creating file
 file=open('v.txt','w') #creates file 
 file.write('Vijayakumar Pattanashetti') #file content
-file.close() #closes file 
 
 #opening file
 file=open('v.txt','r') #opens file
 file.read() #reads full file content
-file.close() #closes file
 
 file=open('v.txt','r') #opens file
 file.read(5) # reads first 5 characters of file content
-file.close() #closes file
 
 #append/edit file
 file=open('v.txt','a') #opens file
-file.write('B.Tech - ECE') #adds these words to already existing content t
-file.close() #closes file
+file.write('B.Tech - ECE') #adds these words to already existing content
 
 #read & write file
 file=open('v.txt','r') #opens file
 file.read() #reads full file content
-file.close() #closes file
