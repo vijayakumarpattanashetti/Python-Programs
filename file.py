342824a9442ac69c47b62f3ca87fb20c1c1fbcdb
vijayakumarpattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2018-04-26 11:54:06 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,14 +1,16 @@
 #creating file
 file=open.('v.txt','w') #creates file 
 file.write('Vijayakumar Pattanashetti') #file content
-file.close() 
+file.close() #closes file 
 
 #opening file
 file=open.('v.txt','r') #opens file
-x=file.read() #reads full file content
-print(x) #prints the read content
-y=file.read(5) # reads first 5 characters of file content
-print(y) #prints the read content
+file.read() #reads full file content
+file.close() #closes file
+
+file=open.('v.txt','r') #opens file
+file.read(5) # reads first 5 characters of file content
+file.close() #closes file
 
 #append/edit file
 file=open.('v.txt','a') #opens file
@@ -17,7 +19,5 @@ file.close() #closes file
 
 #read & write file
 file=open.('v.txt','r') #opens file
-x=file.read() #reads full file content
-print(x)
-y=file.write('Hello..!') # reads first 5 characters of file content
+file.read() #reads full file content
 file.close() #closes file
